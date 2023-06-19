10f92192b2cb85b129ec992b2c08250f665eb393
Kapil Taspa <kapiltaspa@gmail.com>
2023-06-19 18:47:34 -0500

    Update main.java

--- main.java
+++ main.java
@@ -21,4 +21,56 @@ public class main {
         File sourceFolder = new File(sourceFolderPath);
         File[] files = sourceFolder.listFiles();
 
-        
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    String destinationFolderPath = getDestinationFolderPath(file, yearsArray);
+                    File destinationFolder = new File(destinationFolderPath);
+
+                    if (!destinationFolder.exists()) {
+                        destinationFolder.mkdirs();
+                    }
+
+                    Path sourceFilePath = file.toPath();
+                    Path destinationFilePath = new File(destinationFolderPath, file.getName()).toPath();
+
+                    try {
+                        Files.move(sourceFilePath, destinationFilePath, StandardCopyOption.REPLACE_EXISTING);
+                        System.out.println("Moved file: " + file.getName());
+                    } catch (Exception e) {
+                        System.out.println("Failed to move file: " + file.getName());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            System.out.println("No files found in the source folder.");
+        }
+
+        scanner.close();
+    }
+
+    public static String getDestinationFolderPath(File file, String[] yearsArray) {
+        String destinationDirectoryPath = "path/to/destination/directory";
+        String year = "";
+
+        try {
+            Path filePath = file.toPath();
+            BasicFileAttributes fileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            Date creationDate = new Date(fileAttributes.creationTime().toMillis());
+            year = new SimpleDateFormat("yyyy").format(creationDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Check if year matches any specified years
+        for (String specifiedYear : yearsArray) {
+            if (year.equals(specifiedYear.trim())) {
+                return destinationDirectoryPath + File.separator + year;
+            }
+        }
+
+        // If the year doesn't match any specified years, use a default folder
+        return destinationDirectoryPath + File.separator + "Other";
+    }
+}
